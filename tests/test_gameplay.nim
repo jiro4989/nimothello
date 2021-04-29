0d6db6706cc72073d50d900df6cba15cc6b89d05
jiro4989 <jiroron666@gmail.com>
2021-04-29 16:40:08 +0900

    add tests

--- tests/test_gameplay.nim
+++ tests/test_gameplay.nim
@@ -7,12 +7,18 @@ import unittest
 import nimothello/models
 
 var game = newGame()
-game.putCell(6, 4)
-check game.getBoard()[6, 4] == player1
-debugPrint game.getBoard()
-check game.getPlayer1Score() == 4
-check game.getPlayer2Score() == 1
-game.putCell(6, 3)
-check game.getPlayer1Score() == 3
-check game.getPlayer2Score() == 3
-check game.isRunning()
+let tests = [
+  (x: 6, y: 4, scoreP1: 4, scoreP2: 1, status: GameStatus.isRunning),
+  (x: 6, y: 3, scoreP1: 3, scoreP2: 3, status: GameStatus.isRunning),
+  (x: 5, y: 3, scoreP1: 5, scoreP2: 2, status: GameStatus.isRunning),
+  (x: 4, y: 3, scoreP1: 3, scoreP2: 5, status: GameStatus.isRunning),
+  (x: 5, y: 2, scoreP1: 5, scoreP2: 4, status: GameStatus.isRunning),
+  (x: 6, y: 5, scoreP1: 2, scoreP2: 8, status: GameStatus.isRunning),
+  (x: 5, y: 6, scoreP1: 5, scoreP2: 6, status: GameStatus.isRunning),
+]
+for tt in tests:
+  game.putCell(tt.x, tt.y)
+  debugPrint game.getBoard
+  check game.getPlayer1Score() == tt.scoreP1
+  check game.getPlayer2Score() == tt.scoreP2
+  check game.getStatus == tt.status
