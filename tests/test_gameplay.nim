ac7bfa4e176e65f2708b784ce257a8fb8b044d50
jiro4989 <jiroron666@gmail.com>
2021-04-29 16:16:36 +0900

    activate test

--- tests/test_gameplay.nim
+++ tests/test_gameplay.nim
@@ -2,18 +2,17 @@ discard """
   exitcode: 0
 """
 
-when false:
-  import unittest
+import unittest
 
-  import nimothello/models
+import nimothello/models
 
-  var game = newGame()
-  game.putCell(6, 4)
-  check game.getBoard()[6, 4] == player1
-  debugPrint game.getBoard()
-  check game.getPlayer1Score() == 4
-  check game.getPlayer2Score() == 1
-  game.putCell(6, 3)
-  check game.getPlayer1Score() == 3
-  check game.getPlayer2Score() == 3
-  check game.isRunning()
+var game = newGame()
+game.putCell(6, 4)
+check game.getBoard()[6, 4] == player1
+debugPrint game.getBoard()
+check game.getPlayer1Score() == 4
+check game.getPlayer2Score() == 1
+game.putCell(6, 3)
+check game.getPlayer1Score() == 3
+check game.getPlayer2Score() == 3
+check game.isRunning()
