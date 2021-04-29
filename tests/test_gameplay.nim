b913a04aa04f4cbb0423f799b6d2e2746cc48114
jiro4989 <jiroron666@gmail.com>
2021-04-29 16:48:33 +0900

    add tests

--- tests/test_gameplay.nim
+++ tests/test_gameplay.nim
@@ -8,13 +8,17 @@ import nimothello/models
 
 var game = newGame()
 let tests = [
-  (x: 6, y: 4, scoreP1: 4, scoreP2: 1, status: GameStatus.isRunning),
-  (x: 6, y: 3, scoreP1: 3, scoreP2: 3, status: GameStatus.isRunning),
-  (x: 5, y: 3, scoreP1: 5, scoreP2: 2, status: GameStatus.isRunning),
-  (x: 4, y: 3, scoreP1: 3, scoreP2: 5, status: GameStatus.isRunning),
-  (x: 5, y: 2, scoreP1: 5, scoreP2: 4, status: GameStatus.isRunning),
-  (x: 6, y: 5, scoreP1: 2, scoreP2: 8, status: GameStatus.isRunning),
-  (x: 5, y: 6, scoreP1: 5, scoreP2: 6, status: GameStatus.isRunning),
+  (x: 6, y: 4, scoreP1: 4, scoreP2: 1, status: GameStatus.isRunning),  # p1
+  (x: 6, y: 3, scoreP1: 3, scoreP2: 3, status: GameStatus.isRunning),  # p2
+  (x: 5, y: 3, scoreP1: 5, scoreP2: 2, status: GameStatus.isRunning),  # p1
+  (x: 4, y: 3, scoreP1: 3, scoreP2: 5, status: GameStatus.isRunning),  # p2
+  (x: 5, y: 2, scoreP1: 5, scoreP2: 4, status: GameStatus.isRunning),  # p1
+  (x: 6, y: 5, scoreP1: 2, scoreP2: 8, status: GameStatus.isRunning),  # p2
+  (x: 5, y: 6, scoreP1: 5, scoreP2: 6, status: GameStatus.isRunning),  # p1
+  (x: 4, y: 6, scoreP1: 4, scoreP2: 8, status: GameStatus.isRunning),  # p2
+  (x: 3, y: 3, scoreP1: 6, scoreP2: 7, status: GameStatus.isRunning),  # p1
+  (x: 2, y: 3, scoreP1: 3, scoreP2: 11, status: GameStatus.isRunning), # p2
+  (x: 7, y: 4, scoreP1: 7, scoreP2: 8, status: GameStatus.isRunning),  # p1
 ]
 for tt in tests:
   game.putCell(tt.x, tt.y)
